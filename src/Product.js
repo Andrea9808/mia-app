3ee625ff7aaa4faf1f307d4f1122aa370f52293f
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-31 17:32:44 +0200

    feat: event

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,38 @@
-import React from 'react'
+
+//useState, è un hook di React.
+//Permettono di utilizzare lo stato 
+//e altre funzionalità di React all'interno delle funzioni componenti,
+// anziché essere limitati alle classi
+import React, { useState } from 'react';
 
 
   // DESTRUTTURAZIONE 1 + CHILDREN PROPS
 const Product = ({prezzo, img, nome, children}) => {
+
+  // GESTIONE EVENTI (esempio)
+  const handleClick = () => {
+    console.log('bottone premuto')
+  }
+  // GESTIONE EVENTI (esempio)
+  const paramsHandler = (prezzo) => {
+    console.log(prezzo);
+  }
+
+  // GESTIONE EVENTI (esempio)
+  // const eventHandler = (price) => (e) => {
+  //   console.log(e.target.innerText, price);
+
+  // }
+
+  // GESTIONE EVENTI (esempio)
+  function eventHandler(price) {
+    return function(e) {
+      console.log(e.target.innerText, price);
+    }
+  }
+
+  const [background, setBackground] = useState('transparent');
+
 
   // DESTRUTTURAZIONE 2
   // const {nome, img, prezzo} = props
@@ -16,16 +46,26 @@ const Product = ({prezzo, img, nome, children}) => {
   // const alt = "product"
 
   return (
-    <article className='w-25'>
-      <div className='card text-center'>
-        <img  src={img} alt={`${nome}`} />
+    // EVENTO DIRETTAMENTE IN LINEA
+    <article onMouseOver={() => console.log(nome)} className='w-25' >
+      <div  
+        onMouseOver={() => setBackground('red')}
+        onMouseOut={() => setBackground('transparent')}
+        className='w-25'
+        style={{ background }} 
+        className='card text-center'>
+
+        <img  src={img} alt={`${nome}`} onClick={handleClick}/>
         <h3>{nome}</h3>
         <h6>Ne rimangono: {2 * 3}</h6>
 
         {/* CHILDREN PROPS */}
         <p>{children}</p>
         <hr style={hrStyle} />
-        <button className='btn btn-success'>{prezzo}€</button>
+
+        {/* GESTIONE EVENTI */}
+        <button onClick={()=> alert(`nome ${nome}`)} className='btn btn-success'>{prezzo}€</button>
+        <button onClick={eventHandler(prezzo)} className='btn btn-success'>args</button>
       </div>
     </article>
     
